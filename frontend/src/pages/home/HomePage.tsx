d57a819b947a04fd3af2b5b59b94172366deb9b2
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-10-01 10:27:20 +0530

    update

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -10,9 +10,7 @@ const HomePage = () => {
       <Topbar />
       <ScrollArea className="h-[calc(100vh-180px)]">
         <div className="p-4 sm:p-6">
-          <h1 className="text-2xl sm:text-3xl font-bold mb-6">
-            Good afternoon
-          </h1>
+          <h1 className="text-2xl sm:text-3xl font-bold mb-6">hi</h1>
 
           <div className="space-y-8"></div>
         </div>
