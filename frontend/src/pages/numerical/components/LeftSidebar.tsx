dd47a74614498f14dbff532c6fbfdb6c23ac21e5
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-10-01 22:07:37 +0530

    update

--- frontend/src/pages/numerical/components/LeftSidebar.tsx
+++ frontend/src/pages/numerical/components/LeftSidebar.tsx
@@ -1,4 +1,4 @@
-import { Button, buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
 import { SignedIn } from "@clerk/clerk-react";
